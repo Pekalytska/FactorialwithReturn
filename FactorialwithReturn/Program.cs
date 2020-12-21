22f50b2a8bd5786c82d0aa663088492f957e2d02
WIN-O6MLADSHQ11\User <vimmelbuch@gmail.com>
2020-12-21 18:10:21 +0200

    FactorialwithReturn

---
+++ FactorialwithReturn/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static ulong Factorial(uint min, int factorial, ulong iEmul)
+        {
+            if (min <= factorial)
+            {
+                iEmul = iEmul * min;
+                min++;
+                return Factorial(min, factorial, iEmul);
+            }
+            return iEmul;
+        }
+        static void Main(string[] args)
+        {
+            uint min = 1;
+            int factorial = Int32.Parse(Console.ReadLine()); //max 20
+            ulong iEmul = 1;
+            ulong factor = Factorial(min, factorial, iEmul);
+            Console.WriteLine(factor);
+        }
+    }
+}
